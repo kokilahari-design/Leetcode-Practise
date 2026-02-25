dd6068ce581886633e79b3aa6cb5160783d34091
kokilahari-design <kokila2065@gmail.com>
2026-02-25 12:36:05 +0530

    509. Fibonacci Number
    Refactor Fibonacci calculation to use tuple unpacking for clarity and remove unnecessary loop.

--- .github/workflows/Fibonacci Number.py
+++ .github/workflows/Fibonacci Number.py
@@ -1,5 +1,5 @@
 
-# 509. Fibonacci Number :  
+# 509. Fibonacci Number
 # F(0) = 0, F(1) = 1, F(n) = F(n - 1) + F(n - 2), for n > 1.
 # Input: n = 4, Output: 3 (return last index value), Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 
@@ -39,14 +39,9 @@ if n == 0:
 elif n == 1:
     print(1)
 else:
-    prev2 = 0
-    prev1 = 1
-
-    for i in range(2, n + 1):
-        curr = prev1 + prev2
-        prev1 = curr
-        prev2 = prev1
-        
+    prev2, prev1 = 0, 1    # 0,1,1,2,3,5
+    for _ in range(2, n + 1):
+        prev2, prev1 = prev1, prev1 + prev2
 
     print(prev1)
 
